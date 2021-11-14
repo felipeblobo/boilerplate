dbbe17687c5fad0d306d825e06a18f9fc757e597
felipeblobo <felipeblobo@gmail.com>
2021-11-14 18:30:41 -0300

    finishing boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta name="description" content="An next project" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
