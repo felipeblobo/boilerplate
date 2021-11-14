7c97e965f853e1b70ef25ef47cb9a156aa90ca9c
felipeblobo <felipeblobo@gmail.com>
2021-11-14 16:38:50 -0300

    first component created

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,22 @@
+import { Description, Ilustration, Logo, Title, Wrapper } from './styles';
+
 const Main = () => {
   return (
-    <main>
-      <h1>Next practices</h1>
-    </main>
+    <Wrapper>
+      <Logo
+        src="/img/logo.svg"
+        alt="An image with the name React Avançado besides"
+      />
+      <Title>Practicing Next </Title>
+      <Description>
+        A small project to learn Typescript, ReactJS, NextJS and Styled
+        Components
+      </Description>
+      <Ilustration
+        src="/img/hero-illustration.svg"
+        alt="A developer guy working"
+      />
+    </Wrapper>
   );
 };
 
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,8 +5,15 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />);
     expect(
-      screen.getByRole('heading', { name: /Next practices/i }),
+      screen.getByRole('heading', { name: /Practicing next/i }),
     ).toBeInTheDocument();
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b',
+    });
   });
 });
